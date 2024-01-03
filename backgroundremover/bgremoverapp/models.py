1090ad15ce2ca219dc285676bff03ed3a3f074ba
chengivan2 <chengoleivan@gmail.com>
2024-01-03 10:35:04 +0300

    Made first migrations, Created the user feedback model, Created superuser

--- backgroundremover/bgremoverapp/models.py
+++ backgroundremover/bgremoverapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    email_address = models.EmailField(max_length = 254)
+    feedback_text = models.CharField(max_length=200)
