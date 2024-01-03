2371e74d6fe29a7a9e87fc1c5ea20692e7d22eae
chengivan2 <chengoleivan@gmail.com>
2024-01-03 15:23:36 +0300

    Tested other several views. They work OK. Tested remove.bg API request using the example image's URL.

--- backgroundremover/bgremoverapp/urls.py
+++ backgroundremover/bgremoverapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 #Take a look at the urls.py in the root directory
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("removebg", views.bgremover, name="bgremover")
 ]
--- backgroundremover/bgremoverapp/views.py
+++ backgroundremover/bgremoverapp/views.py
@@ -4,3 +4,25 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, world. This is my Background remover app.")
+
+def about(request):
+    return HttpResponse("This is the about page")
+
+def bgremover(request):
+    import requests
+
+    response = requests.post(
+        'https://api.remove.bg/v1.0/removebg',
+        data={
+            'image_url': 'https://www.remove.bg/example.jpg',
+            'size': 'auto'
+        },
+        headers={'X-Api-Key': 'YOUR-API-KEY'},
+    )
+    if response.status_code == requests.codes.ok:
+        with open('no-bg1.png', 'wb') as out:
+            out.write(response.content)
+    else:
+        print("Error:", response.status_code, response.text)
+
+    return HttpResponse(response.status_code)
